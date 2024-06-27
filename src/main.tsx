5d1f92b1675a97b0a143ed62c5f2f3c42165d48f
Aravind Prabash <emailaravindha@gmail.com>
2024-06-27 22:43:06 +0800

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import 'normalize.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,13 @@
 import { Container } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/header/Header';
 
 export default function Root() {
   return (
     <>
+      <Container>
+      </Container>
+      <Header />
       <Container maxWidth="lg">
         <Outlet />
       </Container>
